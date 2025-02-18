b8173e4cce0ff6bfa752537012e45f8e5cb24682
Tineil_A <tine6573@gmail.com>
2025-02-18 02:01:28 -0500

    added another page, validated html again

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 // declare variables to grab DOM elements
+const para = document.getElementsByClassName('story');
 
-// 
+console.log(para);
+// grab text from the DOM elements
+
+// inject the selected element into p.story element
