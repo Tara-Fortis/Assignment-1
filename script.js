708f43d07328a513bb675bb6ef6d4a83aee99bed
Tineil_A <tine6573@gmail.com>
2025-02-18 05:41:31 -0500

    added more code to output a sentence that matches the radio buttons. Very messy. Will try to make it more condensed later.

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
 // grab the radio button output
 const subjectInputs = document.querySelectorAll('input[name="subject"]');
 // console.log(subjectInputs);
-// grab the paragraph element (#story)
-const story = document.querySelector('.story');
+const verbInputs = document.querySelectorAll('input[name="verb"]');
+const adjectiveInputs = document.querySelectorAll('input[name="adjective"]');
+const directObjectInputs = document.querySelectorAll('input[name="directObject"]');
+const indirectObjectInputs = document.querySelectorAll('input[name="indirectObject"]');
+
+// grab a spot for the output
+const subject = document.querySelector('#subject');
+const verb = document.querySelector('#verb');
+const adjective = document.querySelector('#adjective');
+const directObject = document.querySelector('#directObject');
+const indirectObject = document.querySelector('#indirectObject');
 // console.log(story);
 
 for (let i = 0; i < subjectInputs.length; i++){
     subjectInputs[i].addEventListener('change', function(){
-        story.textContent = this.value.replace('-', ' ');
+        subject.textContent = this.value.replace('-', ' ');
     })
 }
 
-// do it for the rest of the document?
-
-const verbInputs = document.querySelectorAll('input[name="verb"]');
+// verb
 for (let i= 0; i <verbInputs.length; i++){
     verbInputs[i].addEventListener('change', function(){
-        story.textContent = this.value.replace('-', ' ');
+        verb.textContent = this.value.replace('-', ' ');
+    })
+}
+
+// adjective
+for (let i= 0; i <adjectiveInputs.length; i++){
+    adjectiveInputs[i].addEventListener('change', function(){
+        adjective.textContent = this.value.replace('-', ' ');
+        //console.log(adjective);
+    })
+}
+
+// directObject
+for (let i= 0; i <directObjectInputs.length; i++){
+    directObjectInputs[i].addEventListener('change', function(){
+        directObject.textContent = this.value.replace('-', ' ')
+        //console.log(this.value);
+    })
+}
+
+// indirectObject
+for (let i= 0; i <indirectObjectInputs.length; i++){
+    indirectObjectInputs[i].addEventListener('change', function(){
+        indirectObject.textContent = this.value.replaceAll('-', ' ');
+        //console.log(this.value);
     })
 }
