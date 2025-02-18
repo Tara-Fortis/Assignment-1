f5e1ba7e8ff4653c7e76849e36dd66a676e7efbc
Tineil_A <tine6573@gmail.com>
2025-02-18 05:03:51 -0500

    deleted story.php, changed index.php to index.html nested input inside of label and changed their attributes, added some js. Just need to figure out how to get it to concatinate its output into one sentence.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// declare variables to grab form output
+// grab the radio button output
+const subjectInputs = document.querySelectorAll('input[name="subject"]');
+// console.log(subjectInputs);
+// grab the paragraph element (#story)
+const story = document.querySelector('.story');
+// console.log(story);
+
+for (let i = 0; i < subjectInputs.length; i++){
+    subjectInputs[i].addEventListener('change', function(){
+        story.textContent = this.value.replace('-', ' ');
+    })
+}
+
+// do it for the rest of the document?
+
+const verbInputs = document.querySelectorAll('input[name="verb"]');
+for (let i= 0; i <verbInputs.length; i++){
+    verbInputs[i].addEventListener('change', function(){
+        story.textContent = this.value.replace('-', ' ');
+    })
+}
